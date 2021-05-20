0dd05f258bf465715bdf0ea23fe502f9e6866b5c
Albert <albertbarsegyan6@gmail.com>
2021-05-20 20:19:47 +0400

    docs(regex.js)

--- tasks05.18/regex.js
+++ tasks05.18/regex.js
@@ -3,5 +3,5 @@ let str = '<> <a href="/"> <input type="radio" checked> <b>';
 str.match(REGEXP);
 '<a href="/">', '<input type="radio" checked>', "<b>"; */
 let str = '<> <a href="/"> <input type="radio" checked> <b>';
-const REGEXP = str.split('<'); //
+const REGEXP = str.split('>'); //
 console.log(REGEXP);
