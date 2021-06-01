2d7fe6781d5c17c36ecfb97efd63153701255302
Albert <albertbarsegyan6@gmail.com>
2021-06-01 09:42:02 +0400

    docs(test3)

--- test3.js
+++ test3.js
@@ -1,5 +1,29 @@
-toArray({ a: 1, b: 2 });
+var gcd = function (first, second) {
+  if (!second) {
+    return first;
+  }
 
-function toArray(obj) {
-  return obj.entries();
+  return gcd(second, first % second);
+};
+// console.log(gcd(2154, 458));
+
+function getPrimeList(number, primeList = []) {
+  if (number <= 1) {
+    return primeList;
+  }
+  let divider = 2;
+  while (divider <= 7) {
+    if (number % divider === 0) {
+      primeList.push(divider);
+      break;
+    } else {
+      divider++;
+    }
+  }
+  if (number % primeList[primeList.length - 1] !== 0) {
+    primeList.push(number);
+    return primeList;
+  }
+  return getPrimeList(number / primeList[primeList.length - 1], primeList);
 }
+console.log(getPrimeList(101));
