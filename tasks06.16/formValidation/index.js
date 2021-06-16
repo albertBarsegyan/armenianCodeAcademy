46a9c8d4b24360c9631b12e3da510529e84fc45c
Albert <albertbarsegyan6@gmail.com>
2021-06-16 23:39:40 +0400

    feat(index.js): add new functions

--- tasks06.16/formValidation/index.js
+++ tasks06.16/formValidation/index.js
@@ -2,30 +2,83 @@ const email = document.querySelector('#email');
 const phone = document.querySelector('#phone');
 const sendButton = document.querySelector('#sendButton');
 const messageContainer = document.querySelector('#messageContainer');
-const messageBlock = document.querySelector('#errorMessage');
-
+const messageBlock = document.querySelector('#messageContainer');
+const phoneNumberContainer = document.querySelector('#phoneContainer');
+const modalContainer = document.querySelector('.modal-container');
+const closeButton = document.querySelector('#closeButton');
+let openModal = 0;
 function emailValid(e) {
   const emailCheckReg =
     /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-  console.log(this.value);
+  // console.log(this.value);
   if (emailCheckReg.test(this.value)) {
     e.target.classList.remove('validation-message--invalid');
     e.target.classList.add('validation-message--success');
+    e.target.style.color = 'rgb(39, 180, 39)';
+    setTimeout(() => {
+      messageBlock.style.opacity = 0;
+    }, 500);
+    setTimeout(() => {
+      messageBlock.style.display = 'none';
+    }, 600);
+    openModal = 1;
   } else {
     e.target.classList.remove('validation-message--success');
     e.target.classList.add('validation-message--invalid');
+    e.target.style.color = 'rgb(240, 41, 41)';
+    messageBlock.style.display = 'block';
+    setTimeout(() => {
+      messageBlock.style.opacity = 1;
+    }, 500);
   }
 }
-function passwordValid(e) {
-  const passwordCheckReg = /^(([374]{4}|[0]{1}))?([1-9]{2})(\d{6})$/;
-  console.log(this.value);
-  if (passwordCheckReg.test(this.value)) {
+function phoneValid(e) {
+  const phoneCheckRed = /(^0\d{8})|^[^0]\d{7}/;
+
+  if (
+    phoneCheckRed.test(this.value) &&
+    [8, 9].includes(e.target.value.length)
+  ) {
+    e.target.style.color = 'rgb(39, 180, 39)';
     e.target.classList.remove('validation-message--invalid');
     e.target.classList.add('validation-message--success');
+    setTimeout(() => {
+      phoneNumberContainer.style.opacity = 0;
+    }, 500);
+    setTimeout(() => {
+      phoneNumberContainer.style.display = 'none';
+    }, 600);
+    ++openModal;
   } else {
+    e.target.style.color = 'rgb(240, 41, 41)';
     e.target.classList.remove('validation-message--success');
     e.target.classList.add('validation-message--invalid');
+    phoneNumberContainer.style.display = 'block';
+    setTimeout(() => {
+      phoneContainer.style.opacity = '1';
+    }, 500);
   }
 }
-email.addEventListener('change', emailValid);
-phone.addEventListener('change', passwordValid);
+
+sendButton.addEventListener('click', function (e) {
+  e.preventDefault();
+  if (openModal === 2) {
+    modalContainer.style.display = 'flex';
+    setTimeout(() => {
+      modalContainer.style.opacity = '1';
+    }, 500);
+    console.log(openModal);
+  } else {
+    console.log(openModal);
+    console.log('dont');
+  }
+});
+
+email.addEventListener('focusout', emailValid);
+phone.addEventListener('focusout', phoneValid);
+closeButton.addEventListener('click', function () {
+  modalContainer.style.opacity = 0;
+  setTimeout(() => {
+    modalContainer.style.display = 'none';
+  }, 600);
+});
