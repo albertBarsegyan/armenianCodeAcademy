58abf37b8696bdd60911fe42534ed66e6e4f271d
Albert <albertbarsegyan6@gmail.com>
2021-06-22 11:06:14 +0400

    feat(timer): add timer functionality

--- tasks06.21/timer/index.js
+++ tasks06.21/timer/index.js
@@ -3,6 +3,7 @@ const startButton = document.querySelector('#startButton');
 const stopButton = document.querySelector('#stopButton');
 const resetButton = document.querySelector('#resetButton');
 
+const messageBlock = document.querySelector('.message');
 // input time
 const hourInput = document.querySelector('#hourInput');
 const minuteInput = document.querySelector('#minuteInput');
@@ -23,25 +24,40 @@ inputTime.forEach((element) => {
 });
 // output
 const timerOutput = document.querySelector('#timerOutput');
+let secondCount;
 // event listener
-startButton.addEventListener('click', (e) => {
+function startEvent(e) {
+  if (messageBlock.style.display === 'block') {
+    messageBlock.style.left = 'calc(0px - 20%)';
+    messageBlock.style.transform = 'translateX(0%)';
+    setTimeout(() => {
+      messageBlock.style.display = 'none';
+    }, 650);
+  }
   if (
     checkIfInputIsTime(hourInput) &&
     checkIfInputIsTime(minuteInput) &&
     checkIfInputIsTime(secondInput)
   ) {
-    const hour = hourInput.value;
-    const minute = minuteInput.value;
-    const second = secondInput.value;
-    // inputTime.forEach((input) => {
-    //   const newElement = document.createElement('span');
-    //   newElement.classList.add('input-time');
-    //   newElement.innerText = input.value;
-    //   input.parentNode.replaceChild(newElement, input);
-    // });
+    inputTime.forEach((input) => {
+      input.setAttribute('disabled', '');
+    });
+    secondCount === undefined
+      ? timerStart(hourInput, minuteInput, secondInput)
+      : null;
   }
-});
+}
 
+startButton.addEventListener('click', startEvent);
+
+stopButton.addEventListener('click', stopEvent);
+resetButton.addEventListener('click', (e) => {
+  stopEvent();
+  inputTime.forEach((input) => {
+    input.removeAttribute('disabled', '');
+    input.value = '';
+  });
+});
 // functions
 function checkIfInputIsTime(input) {
   const timeRegex = /^\d{1,2}/;
@@ -50,4 +66,39 @@ function checkIfInputIsTime(input) {
   } else {
     return false;
   }
+}
+
+function timerStart(hourInput, minuteInput, secondInput) {
+  secondCount = setInterval(() => {
+    if (
+      Number(hourInput.value) === 0 &&
+      Number(minuteInput.value) === 0 &&
+      Number(secondInput.value) === 0
+    ) {
+      clearInterval(secondCount);
+      secondCount = undefined;
+      messageBlock.style.display = 'block';
+      setTimeout(() => {
+        messageBlock.style.left = '50%';
+        messageBlock.style.transform = 'translateX(-50%)';
+        messageBlock.style.opacity = 1;
+      }, 100);
+      return;
+    }
+    secondInput.value -= 1;
+    console.log(Number(secondInput.value));
+    if (Number(secondInput.value) < 0) {
+      secondInput.value = 60;
+      minuteInput.value -= 1;
+    }
+    if (Number(minuteInput.value) < 0) {
+      minuteInput.value = 60;
+      hourInput.value -= 1;
+    }
+  }, 1000);
+}
+
+function stopEvent() {
+  clearInterval(secondCount);
+  secondCount = undefined;
 }
