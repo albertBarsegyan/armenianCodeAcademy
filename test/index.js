6b9fed3ef5d93fd94d4ee73e2ee73268eaef5ae8
Albert <albertbarsegyan6@gmail.com>
2021-06-24 19:59:08 +0400

    feat(test): add new function

--- test/index.js
+++ test/index.js
@@ -1,4 +1,14 @@
-const imageContainer = document.querySelector('#imageContainer');
+const outputPlace = document.querySelector('#jsonData');
 fetch(
-  'https://images.unsplash.com/photo-1624352320183-b2b522225c42?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1601&q=80'
-);
+  'https://images.unsplash.com/photo-1624465737527-4a3d5fe40f06?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=636&q=80'
+).then((res) => {
+  console.log(res.body);
+});
+const promise = new Promise((res, rej) => {
+  if (true) {
+    res(1);
+  }
+});
+promise.then((res) => {
+  console.log(res);
+});
